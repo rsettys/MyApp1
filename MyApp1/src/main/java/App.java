e745996ac442ee96e6aa68eb9a156df09058276d
rsettys <r.setty86@gmail.com>
2020-06-24 11:24:10 +0530

    My First Commit from eclipse

---
+++ MyApp1/src/main/java/App.java
@@ -0,0 +1,12 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Calculator c =new Calculator();
+		System.out.println("Sum: "+c.add(4, 8));
+
+	}
+
+}
---
+++ MyApp1/src/main/java/Calculator.java
@@ -0,0 +1,11 @@
+
+public class Calculator {
+	
+	public int add(int a,int b)
+	{
+		System.out.println("Add Method");
+		return a+b;
+		
+	}
+
+}
